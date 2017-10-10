83a34591a609880ebcde0fda1fc65bfb5c304630
Dmytro Kaidalov <dmtr.kd@gmail.com>
2017-10-10 12:19:59 +0100

    Add unit tests for basic operations. Add json with test cases.

--- src/test/scala/EllipticCurveCryptosystemTest.scala
+++ src/test/scala/EllipticCurveCryptosystemTest.scala
@@ -1,9 +1,18 @@
+import java.util
+
+import org.bouncycastle.util.encoders.{Base64, Hex}
+import org.json.JSONObject
 import org.scalatest.FunSuite
+
+import scala.io.Source
 
 class EllipticCurveCryptosystemTest extends FunSuite {
 
+  val jsonTestData = Source.fromResource("cryptosystem_test.json").mkString
+  val jsonObj = new JSONObject(jsonTestData);
+
   test("encrypt/decrypt message") {
-    val message = math.pow(2, 15).toInt
+    val message = math.pow(2, 10).toInt
     val rand = Array[Byte](100)
 
     val cs = new EllipticCurveCryptosystem
@@ -13,5 +22,37 @@ class EllipticCurveCryptosystemTest extends FunSuite {
     val decryptedMessage = cs.decrypt(privKey, ciphertext)
 
     assert(message == decryptedMessage)
+  }
+
+  test("encryption") {
+    val inputObj = jsonObj.getJSONObject("encryption").getJSONObject("input")
+    val cipherObj = jsonObj.getJSONObject("encryption").getJSONObject("output").getJSONObject("ciphertext")
+
+    val pubkey = Hex.decode(inputObj.getString("pubkey"))
+    val randomness = Hex.decode(inputObj.getString("randomness"))
+    val message = Integer.parseInt(inputObj.getString("message"), 16)
+
+    val ciphertextToCheck = (Hex.decode(cipherObj.getString("c1")), Hex.decode(cipherObj.getString("c2")))
+
+    val cs = new EllipticCurveCryptosystem
+    val ciphertext = cs.encrypt(pubkey, randomness, message)
+
+    assert(util.Arrays.equals(ciphertext._1, ciphertextToCheck._1))
+  }
+
+  test("decryption") {
+    val inputObj = jsonObj.getJSONObject("decryption").getJSONObject("input")
+    val cipherObj = inputObj.getJSONObject("ciphertext")
+    val outputObj = jsonObj.getJSONObject("decryption").getJSONObject("output")
+
+    val privkey = Hex.decode(inputObj.getString("privkey"))
+    val ciphertext = (Hex.decode(cipherObj.getString("c1")), Hex.decode(cipherObj.getString("c2")))
+
+    val messageToCheck = Integer.parseInt(outputObj.getString("message"), 16)
+
+    val cs = new EllipticCurveCryptosystem
+    val message = cs.decrypt(privkey, ciphertext)
+
+    assert(message == messageToCheck)
   }
 }
