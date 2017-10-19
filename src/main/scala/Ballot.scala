27f7823890189943faae35d0e876b4d721b3964f
Dmytro Kaidalov <dmtr.kd@gmail.com>
2017-10-19 14:42:08 +0300

    Create traits for Voter and Ballot. Add stake to Ballot.

--- src/main/scala/Ballot.scala
+++ src/main/scala/Ballot.scala
@@ -2,26 +2,8 @@ import common.Ciphertext
 import org.json.JSONObject
 
 // The data structure for storing of the individual voter's/expert's choice
-class Ballot {
-
-  var issuerID = 0
-  var proposalID = 0
-  var expertsNum = 0
-  val voterChoisesNum = 3
-
-  // Unit vector of expertsNum + voterChoisesNum elements
-  var unitVector: Array[Ciphertext] = null
-
-//  var unitNizks: Array[UnitNIZK] = null
-//  var unitsSumNizk: UnitsSumNIZK = new UnitsSumNIZK
-
-  // Initializes ballot (sets the ID of issuer, the number of experts and creates the unitVector of necessary size)
-  def initialize(_issuerID: Integer, _expertsNum: Integer, _proposalID: Integer): Unit = {
-    issuerID = _issuerID
-    expertsNum = _expertsNum
-    proposalID = _proposalID
-    unitVector = new Array[Ciphertext](expertsNum + voterChoisesNum)
-  }
+sealed trait Ballot {
+  final val VOTER_CHOISES_NUM = 3
 
   // Imports ballot from binary array (parses the binary data, sets the id of issuer, the number of experts and creates unitVector of necessary size and fills it)
   def importJSON(jsonBallot: JSONObject): Unit = ???
@@ -34,4 +16,20 @@ class Ballot {
 
   // Export of the ballot's internal state to binary format for further transmission (main method for exporting, as binary format is most compact)
   def exportBin(): Array[Byte] = ???
+}
+
+case class VoterBallot(val issuerId: Int,
+                       val propodalId: Int,
+                       val expertsNum: Int,
+                       val stake: Int) extends Ballot {
+  // Unit vector of expertsNum + voterChoisesNum elements
+  val unitVector: Array[Ciphertext] = new Array(expertsNum + VOTER_CHOISES_NUM)
+
+//  var unitNizks: Array[UnitNIZK] = null
+//  var unitsSumNizk: UnitsSumNIZK = new UnitsSumNIZK
+}
+
+case class ExpertBallot(val issuerId: Int,
+                        val propodalId: Int) extends Ballot {
+  val unitVector: Array[Ciphertext] = new Array(VOTER_CHOISES_NUM)
 }
--- src/test/scala/VotingProtocolTest.scala
+++ src/test/scala/VotingProtocolTest.scala
@@ -6,10 +6,6 @@ import scala.collection.mutable.ArrayBuffer
 class VotingProtocolTest extends FunSuite {
 
   test("voting") {
-
-    // Simulating the shared database, where all ballots are stored
-    var sharedBallotsList = ArrayBuffer[Ballot]()
-
     // Simulating the shared public and private keys of committees
     val cs = new EllipticCurveCryptosystem
     val (privKey, pubKey) = cs.createKeyPair()
@@ -18,17 +14,22 @@ class VotingProtocolTest extends FunSuite {
     val proposalID = 1
     val expertsNum = 10
 
-    // Simulating the set of experts and voters
-    for (voterId <- 1 to 20) // assume the IDs lower or equal to expertsNum (from 1 to 10) - are experts
-    {
-      val voter = new Voter(cs, voterId, expertsNum, pubKey)
-      val voterBallot = voter.produceVote(proposalID, 0, if(voterId % 2 == 0) VoteCases.Yes else VoteCases.No)
+    val votersBallots =
+      for (voterId <- 11 to 20) yield {
+        new RegularVoter(cs, voterId, expertsNum, pubKey)
+          .produceVote(proposalID, 0, if(voterId % 2 == 0) VoteCases.Yes else VoteCases.No)
+      }
 
-      sharedBallotsList += voterBallot
-    }
+    val expertsBallots =
+      for (expertId <- 1 to 10) yield {
+        new Expert(cs, expertId, expertsNum, pubKey)
+          .produceVote(proposalID, 0, if(expertId % 2 == 0) VoteCases.Yes else VoteCases.No)
+      }
+
+    val ballots = votersBallots ++ expertsBallots
 
     // Obtaining results by an arbitrary voter
-    val voter = new Voter(cs, 0, expertsNum, pubKey)
-    voter.tallyVotes(sharedBallotsList, privKey)
+    val voter = new RegularVoter(cs, 11, expertsNum, pubKey)
+    voter.tallyVotes(ballots, privKey)
   }
 }
