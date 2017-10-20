28889de1ae9467a8d283ae1d58e72a34376ad60b
Dmytro Kaidalov <dmtr.kd@gmail.com>
2017-10-20 16:15:49 +0300

    Add utils for counting time

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -13,4 +13,22 @@ package object common {
   object VoteCases extends Enumeration {
     val Yes, No, Abstain = Value
   }
+
+  object Utils {
+    def time[R](block: => R): R = {
+      val t0 = System.nanoTime()
+      val result = block
+      val t1 = System.nanoTime()
+      println("Elapsed time: " + (t1-t0)/1000000 + " ms")
+      result
+    }
+
+    def time[R](msg: String, block: => R): R = {
+      val t0 = System.nanoTime()
+      val result = block
+      val t1 = System.nanoTime()
+      println(msg + " " + (t1-t0)/1000000 + " ms")
+      result
+    }
+  }
 }
