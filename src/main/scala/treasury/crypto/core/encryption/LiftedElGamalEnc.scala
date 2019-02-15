c91289af7ef1622b436ba4e9df7b3355b12e12af
Dmytro Kaidalov <dmtr.kd@gmail.com>
2019-02-15 11:29:43 +0200

    Implement Lifted Elgamal encryption. Add unit tests

--- src/main/scala/treasury/crypto/core/encryption/LiftedElGamalEnc.scala
+++ src/main/scala/treasury/crypto/core/encryption/LiftedElGamalEnc.scala
@@ -1,14 +1,58 @@
 package treasury.crypto.core.encryption
 
-import treasury.crypto.core.primitives.dlog.DiscreteLogGroup
+import java.math.BigInteger
 
-class LiftedElGamalEnc(val dlogGroup: DiscreteLogGroup) {
+import org.bouncycastle.math.ec.ECPoint
+import treasury.crypto.core.{One, Zero}
+import treasury.crypto.core.primitives.dlog.{DiscreteLogGroup, GroupElement}
 
-}
+import scala.util.Try
 
+/*
+* Implements Lifted ElGamal assymetric encryption scheme where message is represented as a scalar.
+*/
 object LiftedElGamalEnc {
 
-//  def encrypt(dlogGroup: DiscreteLogGroup, pubKey: PubKey, rand: Randomness, msg: Message): DlogCiphertext = {
-//
-//  }
+  /* Message to be encrypted should be in range: msg (mod G) = [0 .. MSG_RANGE-1], G is a group order */
+  val MSG_RANGE = scala.math.pow(2, 16).toInt
+
+  /* Lifted ElGamal is based on classical ElGamal where point for dectyption is derived by epnonentiating group generator to msg */
+  def encrypt(pubKey: PubKey, rand: Randomness, msg: BigInt)(implicit dlogGroup: DiscreteLogGroup): Try[DlogCiphertext] =  {
+    dlogGroup.exponentiate(dlogGroup.groupGenerator, msg).flatMap { p =>
+      ElGamalEnc.encrypt(dlogGroup, pubKey, rand, p)
+    }
+  }
+
+  /*
+  * Decryption envolves solving discrete log for the decrypted point. It is assumed that the used exponent is not
+  * bigger than MAX_INTEGER, otherwise an exception will be thrown.
+  */
+  def decrypt(privKey: PrivKey, ciphertext: DlogCiphertext)(implicit dlogGroup: DiscreteLogGroup): Try[BigInt] = {
+    ElGamalEnc.decrypt(dlogGroup, privKey, ciphertext).flatMap { point =>
+      discreteLog(point)
+    }
+  }
+
+  /* Solve discrete logarithm for m*G. Assuming that the exponent space is not big */
+  private def discreteLog(point: GroupElement)(implicit dlogGroup: DiscreteLogGroup): Try[BigInt] = Try {
+    var P = dlogGroup.groupGenerator
+
+    if (point.equals(dlogGroup.groupIdentity))
+      BigInt(0)
+    else if (point.equals(P))
+      BigInt(1)
+    else {
+      /* exponent is allowed in range 1 .. MSG_RANGE */
+      var exponent = 1
+      while(exponent < MSG_RANGE && P.equals(point) == false) {
+        exponent += 1
+        // we are simply multiplying generator to the accumulator on each iteration since it is faster then exponentiate generator each time
+        P = dlogGroup.multiply(P, dlogGroup.groupGenerator).get
+      }
+      if (exponent < MSG_RANGE)
+        exponent // we found valid exponent!
+      else
+        throw new IllegalArgumentException(s"Can not find discrete logarithm for point $point")
+    }
+  }
 }
--- src/test/scala/treasury/crypto/core/encryption/ElGamalEncTest.scala
+++ src/test/scala/treasury/crypto/core/encryption/ElGamalEncTest.scala
@@ -22,8 +22,39 @@ class ElGamalEncTest extends FunSuite with TableDrivenPropertyChecks with Matche
       val ciphertext = ElGamalEnc.encrypt(group, pubKey, rand, message).get
       val decryptedMsg = ElGamalEnc.decrypt(group, privKey, ciphertext).get
 
-      ciphertext should not be (decryptedMsg)
       decryptedMsg should be (message)
+    }
+  }
+
+  val messages =
+    Table(
+      "message",
+      BigInt(0), BigInt(1), BigInt(2), BigInt(100), BigInt(23534), BigInt(LiftedElGamalEnc.MSG_RANGE-1)
+    )
+
+  test("LiftedElGamalEnc should correctly encrypt and decrypt messages") {
+    forAll(dlogGroups) { implicit group =>
+      val (privKey, pubKey) = createKeyPair.get
+      val rand = group.createRandomNumber
+
+      forAll(messages) { msg =>
+        val ciphertext = LiftedElGamalEnc.encrypt(pubKey, rand, msg).get
+        val decryptedMsg = LiftedElGamalEnc.decrypt(privKey, ciphertext).get
+
+        decryptedMsg should be (msg)
+      }
+    }
+  }
+
+  test("LiftedElGamalEnc should fail to decrypt if a message is not from allowed range") {
+    forAll(dlogGroups) { implicit group =>
+      val (privKey, pubKey) = createKeyPair.get
+      val rand = group.createRandomNumber
+
+      val ciphertext = LiftedElGamalEnc.encrypt(pubKey, rand, LiftedElGamalEnc.MSG_RANGE).get
+      val decryptedMsg = LiftedElGamalEnc.decrypt(privKey, ciphertext)
+
+      decryptedMsg.isFailure should be (true)
     }
   }
 }
