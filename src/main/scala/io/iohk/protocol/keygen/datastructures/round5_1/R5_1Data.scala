52f9bdad2f302d06635c261a96cda25d392ed27b
Dmytro Kaidalov <dmtr.kd@gmail.com>
2020-04-06 12:38:46 +0300

    Implement TallyR2Data. Refactor DKG R5_1Data

--- src/main/scala/io/iohk/protocol/keygen/datastructures/round5_1/R5_1Data.scala
+++ src/main/scala/io/iohk/protocol/keygen/datastructures/round5_1/R5_1Data.scala
@@ -1,74 +1,26 @@
 package io.iohk.protocol.keygen.datastructures.round5_1
 
-import com.google.common.primitives.{Bytes, Ints}
 import io.iohk.core.crypto.primitives.dlog.DiscreteLogGroup
-import io.iohk.core.serialization.{BytesSerializable, Serializer}
-import io.iohk.core.utils.HasSize
-import io.iohk.protocol.keygen.datastructures.round4.{OpenedShare, OpenedShareSerializer}
+import io.iohk.core.serialization.Serializer
+import io.iohk.protocol.keygen.datastructures.round4.OpenedShare
 
 import scala.util.Try
 
-case class R5_1Data(
-                     issuerID:        Int,
-                     violatorsShares: Array[(Int, OpenedShare)] // decrypted share from violator to issuer of this message
-                   )
-  extends HasSize with BytesSerializable {
-
-  override type M = R5_1Data
-  override type DECODER = DiscreteLogGroup
-  override val serializer: Serializer[M, DECODER] = R5_1DataSerializer
-
-  def size: Int = bytes.length
-
-  def canEqual(a: Any): Boolean = a.isInstanceOf[M]
-
-  override def equals(that: Any): Boolean =
-    that match {
-      case that: M => that.canEqual(this) && this.hashCode == that.hashCode
-      case _ => false
-    }
-
-  override def hashCode: Int = {
-
-    import java.util.zip.CRC32
-
-    val checksum = new CRC32
-    checksum.update(bytes, 0, bytes.length)
-    checksum.getValue.toInt
-  }
-}
+/**
+  * This is just a wrapper for ViolatorsSharesData
+  *
+  * @param issuerID
+  * @param violatorsShares
+  */
+case class R5_1Data(override val issuerID: Int,
+                    override val violatorsShares: Array[(Int, OpenedShare)]
+                   ) extends ViolatorsSharesData(issuerID, violatorsShares)
 
 object R5_1DataSerializer extends Serializer[R5_1Data, DiscreteLogGroup] {
+  override def toBytes(obj: R5_1Data): Array[Byte] = ViolatorsSharesDataSerializer.toBytes(obj)
 
-  override def toBytes(obj: R5_1Data): Array[Byte] = {
-
-    val violatorsSharesBytes = obj.violatorsShares.foldLeft(Array[Byte]()){(acc, vs) => acc ++ Ints.toByteArray(vs._1) ++ Ints.toByteArray(vs._2.size) ++ vs._2.bytes}
-
-    Bytes.concat(
-      Ints.toByteArray(obj.issuerID),
-      Ints.toByteArray(obj.violatorsShares.length),
-      violatorsSharesBytes,
-    )
-  }
-
-  override def parseBytes(bytes: Array[Byte], ctxOpt: Option[DiscreteLogGroup]): Try[R5_1Data] = Try {
-    case class IntAccumulator(var value: Int = 0){
-      def plus(i: Int): Int = {value += i; value}
-    }
-
-    val offset = IntAccumulator(0)
-
-    val issuerID = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
-
-    val violatorsSharesLen = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
-
-    val violatorsShares = for (_ <- 0 until violatorsSharesLen) yield {
-      val violatorID = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
-      val violatorsShareBytesLen = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
-      val violatorsShareBytes = bytes.slice(offset.value, offset.plus(violatorsShareBytesLen))
-      (violatorID, OpenedShareSerializer.parseBytes(violatorsShareBytes, ctxOpt).get)
-    }
-
-    R5_1Data(issuerID, violatorsShares.toArray)
+  override def parseBytes(bytes: Array[Byte], decoder: Option[DiscreteLogGroup]): Try[R5_1Data] = Try {
+    val s = ViolatorsSharesDataSerializer.parseBytes(bytes, decoder).get
+    R5_1Data(s.issuerID, s.violatorsShares)
   }
 }
---
+++ src/main/scala/io/iohk/protocol/keygen/datastructures/round5_1/ViolatorsSharesData.scala
@@ -0,0 +1,74 @@
+package io.iohk.protocol.keygen.datastructures.round5_1
+
+import com.google.common.primitives.{Bytes, Ints}
+import io.iohk.core.crypto.primitives.dlog.DiscreteLogGroup
+import io.iohk.core.serialization.{BytesSerializable, Serializer}
+import io.iohk.core.utils.HasSize
+import io.iohk.protocol.keygen.datastructures.round4.{OpenedShare, OpenedShareSerializer}
+
+import scala.util.Try
+
+class ViolatorsSharesData(val issuerID:        Int,
+                          val violatorsShares: Array[(Int, OpenedShare)] // decrypted share from violator to issuer of this message
+                         )
+  extends HasSize with BytesSerializable {
+
+  override type M = ViolatorsSharesData
+  override type DECODER = DiscreteLogGroup
+  override val serializer: Serializer[M, DECODER] = ViolatorsSharesDataSerializer
+
+  def size: Int = bytes.length
+
+  def canEqual(a: Any): Boolean = a.isInstanceOf[M]
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case that: M => that.canEqual(this) && this.hashCode == that.hashCode
+      case _ => false
+    }
+
+  override def hashCode: Int = {
+
+    import java.util.zip.CRC32
+
+    val checksum = new CRC32
+    checksum.update(bytes, 0, bytes.length)
+    checksum.getValue.toInt
+  }
+}
+
+object ViolatorsSharesDataSerializer extends Serializer[ViolatorsSharesData, DiscreteLogGroup] {
+
+  override def toBytes(obj: ViolatorsSharesData): Array[Byte] = {
+
+    val violatorsSharesBytes = obj.violatorsShares.foldLeft(Array[Byte]()){(acc, vs) => acc ++ Ints.toByteArray(vs._1) ++ Ints.toByteArray(vs._2.size) ++ vs._2.bytes}
+
+    Bytes.concat(
+      Ints.toByteArray(obj.issuerID),
+      Ints.toByteArray(obj.violatorsShares.length),
+      violatorsSharesBytes,
+    )
+  }
+
+  override def parseBytes(bytes: Array[Byte], ctxOpt: Option[DiscreteLogGroup]): Try[ViolatorsSharesData] = Try {
+    case class IntAccumulator(var value: Int = 0){
+      def plus(i: Int): Int = {value += i; value}
+    }
+
+    val offset = IntAccumulator(0)
+
+    val issuerID = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
+
+    val violatorsSharesLen = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
+
+    val violatorsShares = for (_ <- 0 until violatorsSharesLen) yield {
+      val violatorID = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
+      val violatorsShareBytesLen = Ints.fromByteArray(bytes.slice(offset.value, offset.plus(4)))
+      val violatorsShareBytes = bytes.slice(offset.value, offset.plus(violatorsShareBytesLen))
+      (violatorID, OpenedShareSerializer.parseBytes(violatorsShareBytes, ctxOpt).get)
+    }
+
+    new ViolatorsSharesData(issuerID, violatorsShares.toArray)
+  }
+}
+
