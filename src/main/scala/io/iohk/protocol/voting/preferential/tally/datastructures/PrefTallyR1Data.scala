314bf23dfaf15ddc6de8e3d460f884b82b3d730c
Dmytro Kaidalov <dmtr.kd@gmail.com>
2020-07-20 18:00:58 +0300

    Implement PrefTallyR1Data

---
+++ src/main/scala/io/iohk/protocol/voting/preferential/tally/datastructures/PrefTallyR1Data.scala
@@ -0,0 +1,68 @@
+package io.iohk.protocol.voting.preferential.tally.datastructures
+
+import com.google.common.primitives.{Bytes, Ints, Shorts}
+import io.iohk.core.crypto.encryption.PubKey
+import io.iohk.core.crypto.encryption.elgamal.ElGamalCiphertext
+import io.iohk.core.crypto.primitives.dlog.{DiscreteLogGroup, GroupElement}
+import io.iohk.core.serialization.{BytesSerializable, Serializer}
+import io.iohk.protocol.CryptoContext
+import io.iohk.protocol.nizk.{ElgamalDecrNIZK, ElgamalDecrNIZKProof, ElgamalDecrNIZKProofSerializer}
+
+import scala.util.Try
+
+case class PrefTallyR1Data(issuerID: Int,
+                           delegDecryptedC1: Seq[(GroupElement, ElgamalDecrNIZKProof)]
+                          ) extends BytesSerializable {
+
+  override type M = PrefTallyR1Data
+  override type DECODER = DiscreteLogGroup
+  override val serializer: Serializer[M, DECODER] = PrefTallyR1DataSerializer
+
+  def validate(ctx: CryptoContext,
+               issuerPubKey: PubKey,
+               encryptedVectorForValidation: Seq[ElGamalCiphertext]): Boolean = Try {
+    import ctx.{group, hash}
+    require(delegDecryptedC1.length == encryptedVectorForValidation.length, "Wrong number of decryptedC1 elements")
+
+    for (i <- encryptedVectorForValidation.indices) {
+      val ciphertext = encryptedVectorForValidation(i)
+      val C1sk = delegDecryptedC1(i)._1
+      val plaintext = ciphertext.c2.divide(C1sk).get
+      val proof = delegDecryptedC1(i)._2
+
+      require(ElgamalDecrNIZK.verifyNIZK(issuerPubKey, ciphertext, plaintext, proof), "Invalid proof")
+    }
+  }.isSuccess
+}
+
+object PrefTallyR1DataSerializer extends Serializer[PrefTallyR1Data, DiscreteLogGroup] {
+
+  override def toBytes(obj: PrefTallyR1Data): Array[Byte] = {
+    val decryptedC1Bytes = obj.delegDecryptedC1.foldLeft(Array[Byte]()) { (acc, c1) =>
+      val point = c1._1.bytes
+      val proof = c1._2.bytes
+      Bytes.concat(acc, Array(point.length.toByte), point, proof)
+    }
+    Bytes.concat(
+      Ints.toByteArray(obj.issuerID),
+      Shorts.toByteArray(obj.delegDecryptedC1.length.toShort), decryptedC1Bytes)
+  }
+
+  override def parseBytes(bytes: Array[Byte], decoder: Option[DiscreteLogGroup]): Try[PrefTallyR1Data] = Try {
+    val group = decoder.get
+    val issuerId = Ints.fromByteArray(bytes.slice(0,4))
+
+    val decryptedC1Len = Shorts.fromByteArray(bytes.slice(4, 6))
+    var pos = 6
+    val decryptedC1 = (0 until decryptedC1Len).map { _ =>
+      val len = bytes(pos)
+      val point = group.reconstructGroupElement(bytes.slice(pos+1, pos+1+len)).get
+      pos = pos + len + 1
+      val proof = ElgamalDecrNIZKProofSerializer.parseBytes(bytes.drop(pos), decoder).get
+      pos = pos + proof.bytes.length
+      (point, proof)
+    }
+
+    PrefTallyR1Data(issuerId, decryptedC1)
+  }
+}
---
+++ src/test/scala/io/iohk/protocol/voting/preferential/tally/datastructures/PrefTallyR1DataTest.scala
@@ -0,0 +1,67 @@
+package io.iohk.protocol.voting.preferential.tally.datastructures
+
+import io.iohk.core.crypto.encryption
+import io.iohk.core.crypto.encryption.elgamal.ElGamalEnc
+import io.iohk.core.crypto.encryption.{PrivKey, PubKey}
+import io.iohk.protocol.CryptoContext
+import io.iohk.protocol.nizk.ElgamalDecrNIZK
+import org.scalatest.FunSuite
+
+class PrefTallyR1DataTest extends FunSuite {
+  val ctx = new CryptoContext(None)
+  import PrefTallyR1DataTest.createRandomDecryptionShares
+  import ctx.{group, hash}
+
+  val (privKey, pubKey) = encryption.createKeyPair.get
+
+  test("check PrefTallyR1Data validation") {
+    val (ciphertexts, decryptionShare) = createRandomDecryptionShares(ctx, pubKey, privKey, 5)
+
+    val r1Data = PrefTallyR1Data(0, decryptionShare)
+    require(r1Data.validate(ctx, pubKey, ciphertexts))
+
+    val wrongCiphertext = ciphertexts.reverse
+    require(r1Data.validate(ctx, pubKey, wrongCiphertext) == false)
+
+    val bogusCiphertext = ElGamalEnc.encrypt(pubKey, group.createRandomGroupElement.get).get._1
+    val bogusProof = ElgamalDecrNIZK.produceNIZK(bogusCiphertext, privKey).get
+    val badR1Data = PrefTallyR1Data(0, (r1Data.delegDecryptedC1.head._1, bogusProof) +: r1Data.delegDecryptedC1.tail)
+    require(badR1Data.validate(ctx, pubKey, ciphertexts) == false)
+  }
+
+  test("PrefTallyR1Data serialization") {
+    val (ciphertexts, decryptionShare) = createRandomDecryptionShares(ctx, pubKey, privKey, 5)
+    val r1Data = PrefTallyR1Data(55, decryptionShare)
+    val bytes = r1Data.bytes
+    val restoredR1Data = PrefTallyR1DataSerializer.parseBytes(bytes, Option(group)).get
+
+    require(restoredR1Data.issuerID == 55)
+    require(restoredR1Data.delegDecryptedC1.size == r1Data.delegDecryptedC1.size)
+    require(restoredR1Data.validate(ctx, pubKey, ciphertexts))
+  }
+}
+
+object PrefTallyR1DataTest {
+
+  def createRandomDecryptionShares(ctx: CryptoContext,
+                                  pubKey: PubKey,
+                                  privKey: PrivKey,
+                                  size: Int) = {
+    import ctx.{group, hash}
+
+    val ciphertexts = for (i <- 1 to size) yield {
+      val plaintext = group.createRandomGroupElement.get
+      val ciphertext = ElGamalEnc.encrypt(pubKey, plaintext).get._1
+      ciphertext
+    }
+
+    val decryptedC1WithProofs =
+      ciphertexts.map { c =>
+        val decryptedC1 = c.c1.pow(privKey).get
+        val proof = ElgamalDecrNIZK.produceNIZK(c, privKey).get
+        (decryptedC1, proof)
+      }
+
+    (ciphertexts, decryptedC1WithProofs)
+  }
+}
